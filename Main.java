917bc6853fce431518bb4faead458d8de33ad1f9
Admin <221250@astanait.edu.kz>
2023-05-21 18:57:15 +0600

    Main is add five elements with integer keys and string values. Then, we use an enhanced for loop to iterate over the tree, and for each entry, we print the key and value

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree<Integer, String> tree = new BinarySearchTree<>();
+        tree.put(5, "Bangchan");
+        tree.put(2, "Jungkook");
+        tree.put(8, "Serkan");
+        tree.put(1, "Suga");
+        tree.put(3, "Felix");
+
+        for (BinarySearchTree.Entry<Integer, String> entry : tree) {
+            System.out.println("Key is " + entry.key + " and value is " + entry.value);
+        }
+    }
+}
+
